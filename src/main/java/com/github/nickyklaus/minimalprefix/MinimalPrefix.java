11ebc29271a0c666aef3bbc039719b019f2fa14a
NickyKlaus <20295301+NickyKlaus@users.noreply.github.com>
2023-02-18 02:54:24 +0400

    minimal prefix

--- src/main/java/com/github/nickyklaus/minimalprefix/MinimalPrefix.java
+++ src/main/java/com/github/nickyklaus/minimalprefix/MinimalPrefix.java
@@ -5,12 +5,17 @@ import java.util.Arrays;
 public class MinimalPrefix {
     public static void main(String[] args) {
         int[] values = { 1, 2, 3, 4, 5 };
-        int sum = 3;
 
-        System.out.println(equalArrays(minimalPrefix(values, sum), new int[]{1,2}));
+        System.out.println(equalArrays(minimalPrefix(values, 3), new int[]{1,2}));
+        System.out.println(equalArrays(minimalPrefix(values, 15), new int[]{1,2,3,4,5}));
+        System.out.println(!equalArrays(minimalPrefix(values, 16), new int[]{1,2,3,4,5}));
+        System.out.println(equalArrays(minimalPrefix(values, 0), new int[]{}));
+
+        values = new int[]{0,0,0,0,0,0};
+        System.out.println(equalArrays(minimalPrefix(values, 0), new int[]{0}));
 
         values = new int[]{};
-        System.out.println(equalArrays(minimalPrefix(values, sum), new int[]{}));
+        System.out.println(equalArrays(minimalPrefix(values, 100), new int[]{}));
     }
 
     static int[] minimalPrefix(int[] values, int sum) {
@@ -21,7 +26,7 @@ public class MinimalPrefix {
             return new int[0];
         }
 
-        while (index < values.length && s <= sum) {
+        while (index < values.length) {
             s += values[index++];
             if (s == sum) {
                 return Arrays.copyOfRange(values, 0, index);
