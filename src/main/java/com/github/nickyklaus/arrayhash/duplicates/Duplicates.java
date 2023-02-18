95da01871df9e77017f92908f253970626c6cdb7
NickyKlaus <20295301+NickyKlaus@users.noreply.github.com>
2023-02-18 20:12:47 +0400

    duplicates

--- src/main/java/com/github/nickyklaus/arrayhash/duplicates/Duplicates.java
+++ src/main/java/com/github/nickyklaus/arrayhash/duplicates/Duplicates.java
@@ -1,27 +1,34 @@
 package com.github.nickyklaus.arrayhash.duplicates;
 
+import java.util.HashSet;
+
 public class Duplicates {
-    /*
-    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-    Example 1:
-
-    Input: nums = [1,2,3,1]
-    Output: true
-    Example 2:
-
-    Input: nums = [1,2,3,4]
-    Output: false
-    Example 3:
-
-    Input: nums = [1,1,1,3,3,4,3,2,4,2]
-    Output: true
-
-    Constraints:
-
-    1 <= nums.length <= 10^5
-    -10^9 <= nums[i] <= 10^9
-     */
+    // Given an integer array nums, return true if any value appears at least twice in the array,
+    // and return false if every element is distinct.
     public static void main(String[] args) {
+        System.out.println(containsDuplicates(new int[]{1,2,3,1}));
+        System.out.println(containsDuplicates(new int[]{1,2,3,4}));
+        System.out.println(containsDuplicates(new int[]{1,1,1,3,3,4,3,2,4,2}));
+        System.out.println(containsDuplicates(new int[]{1,1,1,3,3,4,3,2,4,2}));
+        System.out.println(containsDuplicates(null));
+        System.out.println(containsDuplicates(new int[]{}));
+        System.out.println(containsDuplicates(new int[]{1}));
+        System.out.println(containsDuplicates(new int[]{1,1}));
+    }
 
+    static boolean containsDuplicates(int[] values) {
+        if (values == null || values.length < 2) {
+            return false;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int value : values) {
+            if (set.contains(value)) {
+                return true;
+            }
+            set.add(value);
+        }
+
+        return false;
     }
 }
